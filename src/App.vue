753caa2ab347fe3272f7e6eded80ec0aec7c49e5
Rejaul <rejauldu@gmail.com>
2021-11-19 18:21:18 +0600

    Assets added

--- src/App.vue
+++ src/App.vue
@@ -10,3 +10,38 @@
     <cart></cart>
   </div>
 </template>
+<style>
+	.vh {
+		min-height: 100vh;
+	}
+	.vh-50 {
+		position: fixed;
+		right: 0;
+		top:50%;
+		transform: translate(0, -50%);
+	}
+	.weight-25 {
+		width:25px;
+		text-align: center;
+	}
+	.height-25 {
+		height:25px;
+		line-height: 25px;
+	}
+	.grad {
+		background-image: linear-gradient(to right, #B6793F, #ff59af);
+	}
+	.text-warning-deep {
+		color: #B6793F;
+	}
+	.text-danger-deep {
+		color: #ff59af;
+	}
+	.bg-warning-deep {
+		background: #B6793F;
+	}
+	.border-warning-deep {
+		border-color: #B6793F !important;
+		border-width:2px !important;
+	}
+</style>
